b9c9704f38fecc1d1d182f391e50bb9f9b53c22f
Berthelot Loïc <loic.berthelot1@gmail.com>
2017-10-05 11:02:41 +0200

    add folder images

--- globe/globe.js
+++ globe/globe.js
@@ -107,7 +107,7 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture('http://api.meetme.loicberthelot.com/images/world.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture('./images/world.jpg');
   //  uniforms['texture'].value = THREE.ImageUtils.loadTexture('http://localhost:3001/images/world.jpg');
 
      material = new THREE.ShaderMaterial({
